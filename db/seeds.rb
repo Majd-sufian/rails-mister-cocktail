4b7fa4e09e82b479e56ab4c3808a5bd335446d99
Majd Sufian <majdnewsufian@gmail.com>
2020-02-20 15:22:25 +0100

    Add pictures to Cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+puts 'Clearing Ingredients'
+
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+cocktails_serialized = open(url).read
+cocktails = JSON.parse(cocktails_serialized)
+
+cocktails["drinks"].each do |cocktail|
+  Cocktail.create(name: cocktail["strDrink"])
+end
